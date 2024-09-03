5edda23e2cacf5f19f21c4f36e34be0c5be76d3f
Freddie <freddie@theprojectfactory.com>
2024-09-03 16:05:35 +1000

    build color mode switch, add Tailwind

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,12 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import logo from "../../assets/logo.webp";
+import ColorModeSwitch from "../ColorModeSwitch/ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" className="pr-3">
       <Image src={logo} boxSize="60px" />
-      <Text>NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
